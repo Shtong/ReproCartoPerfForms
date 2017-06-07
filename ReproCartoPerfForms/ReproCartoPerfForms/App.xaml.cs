ea5240479fe41e68d509ec54af33e0b6a774a1b8
Shtong <shtong@mm.st>
2017-06-07 17:05:10 +0200

    Initial commit

---
+++ ReproCartoPerfForms/ReproCartoPerfForms/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Xamarin.Forms;
+
+namespace ReproCartoPerfForms
+{
+	public partial class App : Application
+	{
+        public const string CITY_FILE = "rome_ntvt.mbtiles";
+        public const double CITY_POS_X = 12.4807;
+        public const double CITY_POS_Y = 41.8962;
+        
+        public App ()
+		{
+			InitializeComponent();
+            
+            // Extract mbtiles from assets
+            var t = PrepareAsset(CITY_FILE);
+            t.Wait();
+            CityDataFile = t.Result;
+
+            // Start
+			MainPage = new Fast();
+		}
+
+        public string CityDataFile { get; private set; }
+        
+        private async Task<string> PrepareAsset(string assetName)
+        {
+
+            string destPath = Path.Combine(ApplicationData.Current.LocalFolder.Path, assetName);
+
+            if (!File.Exists(destPath))
+            {
+                Debug.WriteLine("PLEASE WAIT! Copying mbtiles...");
+                StorageFile sourceFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/" + assetName, UriKind.Absolute));
+                using (Stream sourceStream = await sourceFile.OpenStreamForReadAsync())
+                {
+                    using (FileStream destStream = File.Create(destPath))
+                    {
+                        await sourceStream.CopyToAsync(destStream);
+                    }
+                }
+                Debug.WriteLine("Copying mbtiles done!");
+            }
+
+            return destPath;
+        }
+    }
+}
---
+++ ReproCartoPerfForms/ReproCartoPerfForms/MapContainer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Carto.Ui;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using System.Diagnostics;
+using Carto.DataSources;
+using Carto.Styles;
+using Carto.Utils;
+using Carto.VectorTiles;
+using Carto.Layers;
+using Carto.Core;
+using System.Threading.Tasks;
+using System.IO;
+using Windows.Storage;
+using Windows.UI.Xaml;
+
+namespace ReproCartoPerfForms
+{
+    public class MapContainer : ContentView
+    {
+        public MapContainer()
+        {
+            Map = new MapView();
+            var timer = new Stopwatch();
+            timer.Start();
+
+            // NOTE: Uncomment the timer creation and BeginInvokeOnMainThread to apply a workaround
+            //var startTimer = new System.Threading.Timer((_) =>
+            //{
+            //    Device.BeginInvokeOnMainThread(() =>
+            //    {
+            MBTilesTileDataSource testDataSource = new MBTilesTileDataSource(((App)App.Current).CityDataFile);
+                    CompiledStyleSet styleSet = new CompiledStyleSet(new ZippedAssetPackage(AssetUtils.LoadAsset("nutibright-v3.zip")));
+                    MBVectorTileDecoder decoder = new MBVectorTileDecoder(styleSet);
+                    VectorTileLayer testLayer = new VectorTileLayer(testDataSource, decoder);
+                    Map.Layers.Add(testLayer);
+            //    });
+            //}, null, 10, System.Threading.Timeout.Infinite);
+
+            timer.Stop();
+            Debug.WriteLine($"Done map initialization in {timer.ElapsedMilliseconds}ms");
+
+            MapPos pos = Map.Options.BaseProjection.FromWgs84(new MapPos(App.CITY_POS_X, App.CITY_POS_Y));
+            Map.SetFocusPos(pos, 0);
+            Map.SetZoom(15, 0);
+
+
+            Content = Map.ToView();
+        }
+
+        public MapView Map { get; private set; }
+        
+    }
+}
